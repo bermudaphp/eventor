064ad4693e1ba0a4c6359315cae3f3cb2762cf46
Andrey <alestro@bk.ru>
2020-06-25 11:04:53 +0200

    Update Event.php

--- src/Event.php
+++ src/Event.php
@@ -11,31 +11,23 @@ use Psr\EventDispatcher\StoppableEventInterface;
  * Class Event
  * @package Lobster\Events
  */
-class Event implements StoppableEventInterface {
+class Event implements StoppableEventInterface 
+{
+    private bool $stop = false;
 
     /**
-     * @var bool
+     * @inheritDoc
      */
-    private $stop = false;
-
-    /**
-     * Is propagation stopped?
-     *
-     * This will typically only be used by the Dispatcher to determine if the
-     * previous listener halted propagation.
-     *
-     * @return bool
-     *   True if the Event is complete and no further listeners should be called.
-     *   False to continue calling listeners.
-     */
-    public function isPropagationStopped() : bool {
-        return $this->stop;
-    }
-
-    /**
-     * Stops the propagation of the event to further event listeners.
-     */
-    public function stopPropagation() : void {
+    public function stopPropagation() : void 
+    {
         $this->stop = true;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function isPropagationStopped() : bool 
+    {
+        return $this->stop;
     }
 }
