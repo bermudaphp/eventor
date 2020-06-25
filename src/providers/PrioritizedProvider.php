11bc657fb551cec376ff22cd254697ce84cbc196
Andrey <alestro@bk.ru>
2020-06-25 10:40:11 +0200

    Update PrioritizedProvider.php

--- src/providers/PrioritizedProvider.php
+++ src/providers/PrioritizedProvider.php
@@ -11,34 +11,20 @@ use Lobster\Events\SubscriberInterface;
  * Class PrioritizedProvider
  * @package Lobster\Events\Providers
  */
-class PrioritizedProvider extends Provider implements PrioritizedProviderInterface {
-
-    /**
-     * @var string
-     */
-    private $name;
-
-    /**
-     * @var array
-     */
-    private $listeners = [];
-
-    /**
-     * PriorityListenerProvider constructor.
-     * @param string $name
-     */
-    public function __construct(string $name) {
-        $this->name = $name;
-    }
-
+class PrioritizedProvider extends Provider implements PrioritizedProviderInterface
+{
     /**
      * @param string $eventType
      * @param callable $listener
      */
-    public function detach(string $eventType, callable $listener): void {
-        if(array_key_exists($eventType, $this->listeners)){
-            foreach ($this->listeners[$eventType] as $i => $v){
-                if($listener === $v['listener']){
+    public function detach(string $eventType, callable $listener): void 
+    {
+        if(array_key_exists($eventType, $this->listeners))
+        {
+            foreach ($this->listeners[$eventType] as $i => $v)
+            {
+                if($listener === $v['listener'])
+                {
                     unset($this->listeners[$eventType][$i]);
                     return;
                 }
@@ -50,22 +36,27 @@ class PrioritizedProvider extends Provider implements PrioritizedProviderInterfa
      * @param object $event
      * @return array[callable]
      */
-    public function getListenersForEvent(object $event) : array {
-
+    public function getListenersForEvent(object $event) : array 
+    {
         $listeners = [];
 
-        foreach ($this->listeners as $eventType => $v){
-            if($event instanceof $eventType){
+        foreach ($this->listeners as $eventType => $v)
+        {
+            if($event instanceof $eventType)
+            {
                 $listeners = array_merge($listeners, $v);
             }
         }
 
-        if($listeners !== []){
-            usort($listeners, function ($a, $b) {
+        if($listeners !== [])
+        {
+            usort($listeners, function ($a, $b)
+            {
                 return $b['priority'] <=> $a['priority'];
             });
 
-            foreach ($listeners as &$listener){
+            foreach ($listeners as &$listener)
+            {
                 $listener = $listener['listener'];
             }
         }
@@ -78,31 +69,24 @@ class PrioritizedProvider extends Provider implements PrioritizedProviderInterfa
      * @param callable $listener
      * @param int $priority
      */
-    public function listen(string $eventType, callable $listener, int $priority = 0): void {
-        if(array_key_exists($eventType, $this->listeners)){
-            foreach ($this->listeners[$eventType] as $v){
-                if($listener === $v['listener']){
+    public function listen(string $eventType, callable $listener, int $priority = 0): void 
+    {
+        if(array_key_exists($eventType, $this->listeners))
+        {
+            foreach ($this->listeners[$eventType] as $v)
+            {
+                if($listener === $v['listener'])
+                {
                     return;
                 }
             }
-        } else {
+        } 
+        
+        else 
+        {
             $this->listeners[$eventType] = [];
         }
 
         $this->listeners[$eventType][] = compact('listener', 'priority');
-    }
-
-    /**
-     * @param SubscriberInterface $subscriber
-     */
-    public function subscribe(SubscriberInterface $subscriber): void {
-        $subscriber->getListeners();
-    }
-
-    /**
-     * @return string
-     */
-    public function getName(): string {
-       return $this->name;
     }
 }
