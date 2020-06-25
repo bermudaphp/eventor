c72505020824a5f0afd56044790262e3181e11af
Andrey <alestro@bk.ru>
2020-06-25 10:09:18 +0200

    Update LazyListener.php

--- src/LazyListener.php
+++ src/LazyListener.php
@@ -11,56 +11,25 @@ use Psr\Container\ContainerInterface;
  * Class LazyListener
  * @package Lobster\Events
  */
-class LazyListener {
+class LazyListener
+{
+    private string $service;
+    private ?string $method;
+    private ContainerInterface $container;
 
-    /**
-     * @var string|null
-     */
-    private $method;
-
-    /**
-     * @var string
-     */
-    private $service;
-
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
-
-    /**
-     * @param ContainerInterface $container
-     * @param string $service
-     * @param string|null $method
-     * @return LazyListener
-     */
-    public static function create(ContainerInterface $container, string $service, string $method = null) : self {
-        return new static($container, $service, $method);
-    }
-
-    /**
-     * LazyListener constructor.
-     * @param ContainerInterface $container
-     * @param string $service
-     * @param string $method
-     */
-    public function __construct(ContainerInterface $container, string $service, string $method = null) {
-        $this->method = $method;
-        $this->service = $service;
+    public function __construct(ContainerInterface $container, string $service, string $method = null)
+    {
+        $this->method    = $method;
+        $this->service   = $service;
         $this->container = $container;
     }
 
     /**
      * @param object $event
      */
-    public function __invoke(object $event) : void {
-
-        $listener = $this->container->get($this->service);
-
-        if($this->method !== null){
-            $listener = [$listener, $this->method];
-        }
-
-        $listener($event);
+    public function __invoke(object $event) : void 
+    {
+        $listener = $this->container->get($this->service); 
+        !$this->method ? $listener($event) : ([$listener, $this->method])($event)
     }
 }
