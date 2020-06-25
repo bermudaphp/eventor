1e40c1d0498349c34034be13e9fbd3b76e3a097a
Andrey <alestro@bk.ru>
2020-06-25 10:34:13 +0200

    Update Provider.php

--- src/providers/Provider.php
+++ src/providers/Provider.php
@@ -5,37 +5,33 @@ namespace Lobster\Events\Providers;
 
 
 use Lobster\Events\ListenerProvider;
-use Lobster\Events\SubscriberInterface;
 
 
 /**
  * Class Provider
  * @package Lobster\Events\Providers
  */
-class Provider implements ListenerProvider {
+class Provider implements ListenerProvider
+{
+    protected string $name;
+    private array $listeners = [];
 
-    /**
-     * @var string
-     */
-    private $name;
-
-    /**
-     * @var array
-     */
-    private $listeners = [];
-
-    /**
-     * ListenerProvider constructor.
-     * @param string $name
-     * @param iterable $listeners
-     */
-    public function __construct(string $name, iterable $listeners = []) {
-
+    public function __construct(string $name, iterable $listeners = [])
+    {
         $this->name = $name;
 
-        foreach ($listeners as $type => $listener){
+        foreach ($listeners as $type => $listener)
+        {
             $this->listen($type, $listener);
         }
+    }
+    
+    /**
+     * @return string
+     */
+    public function getName(): string 
+    {
+        return $this->name;
     }
 
     /**
@@ -45,13 +41,15 @@ class Provider implements ListenerProvider {
      *   An iterable (array, iterator, or generator) of callables.  Each
      *   callable MUST be type-compatible with $event.
      */
-    public function getListenersForEvent(object $event) : array {
-
+    public function getListenersForEvent(object $event) : array 
+    {
         $listeners = [];
 
-        foreach ($this->listeners as $eventName => $listener){
-            if($event instanceof $eventName){
-                $listeners = array_merge($listeners, $listener);
+        foreach ($this->listeners as $eventType => $listener)
+        {
+            if($event instanceof $eventType)
+            {
+                $listeners[] = $listener;
             }
         }
 
@@ -62,7 +60,8 @@ class Provider implements ListenerProvider {
      * @param string $eventType
      * @return bool
      */
-    public function hasListeners(string $eventType) : bool {
+    public function hasListeners(string $eventType) : bool 
+    {
         return array_key_exists($eventType, $this->listeners);
     }
 
@@ -70,25 +69,11 @@ class Provider implements ListenerProvider {
      * @param string $eventType
      * @param callable $listener
      */
-    public function listen(string $eventType, callable $listener) : void {
-        if(!in_array($listener, $this->listeners[$eventType] ?? [], true)){
+    public function listen(string $eventType, callable $listener) : void 
+    {
+        if(!in_array($listener, $this->listeners[$eventType] ?? [], true))
+        {
             $this->listeners[$eventType][] = $listener;
         }
-    }
-
-    /**
-     * @return string
-     */
-    public function getName(): string {
-        return $this->name;
-    }
-
-    /**
-     * @param SubscriberInterface $subscriber
-     */
-    public function subscribe(SubscriberInterface $subscriber): void {
-        foreach ($subscriber->getListeners() as $eventType => $listener){
-            $this->listen($eventType, $listener);
-        };
     }
 }
