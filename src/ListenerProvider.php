2d7a67fef673304a946e6deee062e544e03685ff
Andrey <alestro@bk.ru>
2020-06-25 10:16:01 +0200

    Update ListenerProvider.php

--- src/ListenerProvider.php
+++ src/ListenerProvider.php
@@ -11,21 +11,17 @@ use Psr\EventDispatcher\ListenerProviderInterface;
  * Interface ListenerProviderInterface
  * @package Lobster\Events
  */
-interface ListenerProvider extends ListenerProviderInterface {
-
+interface ListenerProvider extends ListenerProviderInterface 
+{
+    /**
+     * @return string
+     */
+    public function getName() : string ;
+    
     /**
      * @param string $eventType
      * @param callable $listener
      */
     public function listen(string $eventType, callable $listener) : void ;
-
-    /**
-     * @return string
-     */
-    public function getName() : string ;
-
-    /**
-     * @param SubscriberInterface $subscriber
-     */
-    public function subscribe(SubscriberInterface $subscriber) : void ;
+   
 }
