9970415d44f0223c034285c43092c1d2504fd251
Andrey <alestro@bk.ru>
2020-06-30 15:18:25 +0200

    Update and rename PrioritizedProviderInterface.php to PrioritizedListenerProviderInterface.php

---
+++ src/providers/PrioritizedListenerProviderInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Bermuda\Eventor\Provider;
+
+
+use Bermuda\Eventor\ListenerProviderInterface;
+
+
+/**
+ * Interface PrioritizedListenerProviderInterface
+ * @package Bermuda\Eventor\Provider
+ */
+interface PrioritizedListenerProviderInterface extends ListenerProviderInterface
+{
+    /**
+     * @param string $eventType
+     * @param callable $listener
+     * @param int $priority
+     */
+    public function listen(string $eventType, callable $listener, int $priority = 0) : void;
+}
