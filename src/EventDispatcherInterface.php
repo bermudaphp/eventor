f7817c65a37726395ed87467c560238b61a88723
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-03-05 16:11:13 +0300

    Update EventDispatcherInterface.php

--- src/EventDispatcherInterface.php
+++ src/EventDispatcherInterface.php
@@ -10,5 +10,12 @@ use Psr\EventDispatcher\EventDispatcherInterface as PsrDispatcherInterface;
  */
 interface EventDispatcherInterface extends PsrDispatcherInterface
 {
+    /**
+     * The dispatcher must be immutable; This method should be implemented in such 
+     * a way as to keepthe state of the current instance unchanged and 
+     * return an instance with a changed state.
+     * @param ListenerProviderInterface $provider
+     * @return EventDispatcherInterface
+     */
     public function attach(ListenerProviderInterface $provider): EventDispatcherInterface ;
 }
