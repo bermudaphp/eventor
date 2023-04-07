3c29b765d871d0bebce5fe14d9e448defdbd5f61
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-04-07 13:36:29 +0200

    Update Event.php

--- src/Event.php
+++ src/Event.php
@@ -6,6 +6,7 @@ use Psr\EventDispatcher\StoppableEventInterface;
 
 class Event implements StoppableEventInterface 
 {
+    private array $data = [];
     private bool $propagationStopped = false;
 
     /**
@@ -22,5 +23,21 @@ class Event implements StoppableEventInterface
     public function isPropagationStopped(): bool 
     {
         return $this->propagationStopped;
+    }
+
+    public function set(string $key, $value): Event
+    {
+        $this->data[$key] = $value;
+        return $this;
+    }
+    
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $this->data);
+    }
+    
+    public function get(string $key, $default = null)
+    {
+        return $this->data[$key] ?? $default;
     }
 }
