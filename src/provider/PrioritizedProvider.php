7052ef8632ce4733f59f8c3344c2bb3a0bb75bf1
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-04-07 13:17:06 +0200

    Update PrioritizedProvider.php

--- src/provider/PrioritizedProvider.php
+++ src/provider/PrioritizedProvider.php
@@ -2,39 +2,25 @@
 
 namespace Bermuda\Eventor\Provider;
 
-/**
- * Class PrioritizedProvider
- * @package LBermuda\Eventor\Provider
- */
 class PrioritizedProvider extends Provider implements PrioritizedListenerProviderInterface
 {
     /**
      * @param object $event
-     * @return array[callable]
+     * @return callable[]
      */
-    public function getListenersForEvent(object $event) : array 
+    public function getListenersForEvent(object $event): array
     {
         $listeners = [];
-
-        foreach ($this->listeners as $eventType => $v)
-        {
-            if ($event instanceof $eventType)
-            {
-                $listeners += $v;
-            }
+        foreach ($this->listeners as $eventType => $v) {
+            if ($event instanceof $eventType) $listeners += $v;
         }
 
-        if ($listeners !== [])
-        {
-            usort($listeners, static function (array $a, array $b)
-            {
+        if ($listeners !== []) {
+            usort($listeners, static function (array $a, array $b) {
                 return $b['priority'] <=> $a['priority'];
             });
 
-            foreach ($listeners as &$listener)
-            {
-                $listener = $listener['listener'];
-            }
+            foreach ($listeners as &$listener) $listener = $listener['listener'];
         }
 
         return $listeners;
@@ -47,21 +33,9 @@ class PrioritizedProvider extends Provider implements PrioritizedListenerProvide
      */
     public function listen(string $eventType, callable $listener, int $priority = 0): void 
     {
-        if (array_key_exists($eventType, $this->listeners))
-        {
-            foreach ($this->listeners[$eventType] as $v)
-            {
-                if ($listener === $v['listener'])
-                {
-                    return;
-                }
-            }
-        } 
-        
-        else 
-        {
-            $this->listeners[$eventType] = [];
-        }
+        if (array_key_exists($eventType, $this->listeners)) {
+            foreach ($this->listeners[$eventType] as $v) if ($listener === $v['listener']) return;
+        } else $this->listeners[$eventType] = [];
 
         $this->listeners[$eventType][] = compact('listener', 'priority');
     }
